ec814d356a197d008ab4d456ca1aab518e63452b
filippo199600 <pippotartaglia@gmail.com>
2023-07-24 16:32:48 +0200

    js add prompt and cycle for

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,4 +1,5 @@
-emailIndex = [
+// CREO UN ARRAY CON LE VARIE MAIL
+const emailIndex = [
   "admin@email.com",
   "example@email.com",
   "admin@libero.it",
@@ -6,3 +7,18 @@ emailIndex = [
   "admin@america.us",
 ];
 console.table(emailIndex);
+
+// CHIEDO ALL'UTENTE LA MAIL CHE VUOLE INSERIRE
+const userEmail = prompt("inserici una mail");
+console.log(userEmail);
+
+// INSERICO UN CONTROLLO DELLE MAIL DISPONIBILI
+for (let i = 0; i < emailIndex.length; i++) {
+  console.log("variabile i " + i);
+
+  if (userEmail === emailIndex) {
+    console.log("la mail è corretta");
+  } else {
+    console.log("la mail è sbagliata");
+  }
+}
